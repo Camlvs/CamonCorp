2e066df2e32946f69f2a6f24ed2daeec86407df4
McFly <matthieu.roman2002@gmail.com>
2025-09-17 18:27:46 +0200

    remove animation

--- app/page.tsx
+++ app/page.tsx
@@ -489,9 +489,9 @@ export default async function Home() {
 			<div className="px-3 lg:px-0 py-[200px] lg:max-w-[1000px] lg:mx-auto text-center relative">
 				<img src="hautGauche.svg" alt="" className="absolute top-20 left-4 lg:left-0 w-[50px] lg:w-[120px]"/>
 				<img src="basDroite.svg" alt="" className="absolute bottom-20 right-4  lg:right-0 w-[50px] lg:w-[120px]"/>
-				<AnimatedGradientText className="text-[42px] lg:text-[70px] italic font-semibold w-full">
+				<p className="text-[42px] lg:text-[70px] italic font-semibold w-full">
 					On ne suit pas les tendances, on les crée. Prêt à marquer YouTube ?
-				</AnimatedGradientText>
+				</p>
 			</div>
       <div
         id="process"
--- components/magicui/animated-gradient-text.tsx
+++ components/magicui/animated-gradient-text.tsx
@@ -2,36 +2,33 @@ import { cn } from "@/lib/utils";
 import { ComponentPropsWithoutRef } from "react";
 
 export interface AnimatedGradientTextProps
-  extends ComponentPropsWithoutRef<"div"> {
-  speed?: number;
-  colorFrom?: string;
-  colorTo?: string;
+	extends ComponentPropsWithoutRef<"div"> {
+	colorFrom?: string;
+	colorTo?: string;
 }
 
 export function AnimatedGradientText({
-  children,
-  className,
-  speed = 1,
-  colorFrom = "#ECD6B1",
-  colorTo = "#E50C00",
-  ...props
-}: AnimatedGradientTextProps) {
-  return (
-    <span
-      style={
-        {
-          "--bg-size": `${speed * 300}%`,
-          "--color-from": colorFrom,
-          "--color-to": colorTo,
-        } as React.CSSProperties
-      }
-      className={cn(
-        `block w-full animate-gradient bg-gradient-to-r from-[var(--color-from)] via-[var(--color-to)] to-[var(--color-from)] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
-        className
-      )}
-      {...props}
-    >
+																			 children,
+																			 className,
+																			 colorFrom = "#ECD6B1",
+																			 colorTo = "#E50C00",
+																			 ...props
+																		 }: AnimatedGradientTextProps) {
+	return (
+		<span
+			style={
+				{
+					"--color-from": colorFrom,
+					"--color-to": colorTo,
+				} as React.CSSProperties
+			}
+			className={cn(
+				`block w-full bg-gradient-to-r from-[var(--color-from)] to-[var(--color-to)] bg-clip-text text-transparent`,
+				className
+			)}
+			{...props}
+		>
       {children}
     </span>
-  );
+	);
 }
